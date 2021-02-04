8e02ba40d16ae1ac426f7bce11bba38897871443
JordanT111596 <JordanT111596@gmail.com>
2021-02-04 13:44:14 -0500

    created app framework

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
