bc67a20ceb6468afbda6db67e938d42c90168685
JordanT111596 <JordanT111596@gmail.com>
2021-02-04 14:28:45 -0500

    Created exception handling examples for later, created account history function

--- Program.cs
+++ Program.cs
@@ -6,8 +6,37 @@ namespace CSharpBank
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Jordan", 1000);
+            var account = new BankAccount("Jordan", 10000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with ${account.Balance} initial balance.");
+
+            account.MakeWithdrawal(1100, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            account.MakeWithdrawal(60, DateTime.Now, "Bought Ghost of Tsushima");
+
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
